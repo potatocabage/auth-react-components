caa549eaddcdf36a7bb300156d2414e373af82a7
warrickhe <43077069+warrickhe@users.noreply.github.com>
2022-01-22 17:22:11 -0800

    Update InnerLayout.tsx

--- src/components/InnerLayout.tsx
+++ src/components/InnerLayout.tsx
@@ -8,80 +8,80 @@ import { confirmAlert } from 'react-confirm-alert'; // Import
 import 'react-confirm-alert/src/react-confirm-alert.css' // Import css
 
 export interface Preferences {
-  collapsed: boolean,
-  dark: boolean,
+    collapsed: boolean,
+    dark: boolean,
 }
 
 function getDefaultPreferences() {
-  const defaultPreferences: Preferences = {
-    collapsed: true,
-    dark: true,
-  }
-  return defaultPreferences;
+    const defaultPreferences: Preferences = {
+        collapsed: true,
+        dark: true,
+    }
+    return defaultPreferences;
 }
 
 function getPreexistingPreferences() {
-  // what preferences default to on first login
+    // what preferences default to on first login
 
-  const preexistingPreferencesString = localStorage.getItem("preferences");
-  const defaultPreferences = getDefaultPreferences();
+    const preexistingPreferencesString = localStorage.getItem("preferences");
+    const defaultPreferences = getDefaultPreferences();
 
-  if (preexistingPreferencesString == null) {
-    return defaultPreferences;
-  } else {
-    try {
-      // TODO validate here
-      return JSON.parse(preexistingPreferencesString) as Preferences;
-    } catch (e) {
-      // try to clean up a bad config
-      localStorage.setItem("preferences", JSON.stringify(defaultPreferences));
-      return defaultPreferences;
+    if (preexistingPreferencesString == null) {
+        return defaultPreferences;
+    } else {
+        try {
+            // TODO validate here
+            return JSON.parse(preexistingPreferencesString) as Preferences;
+        } catch (e) {
+            // try to clean up a bad config
+            localStorage.setItem("preferences", JSON.stringify(defaultPreferences));
+            return defaultPreferences;
+        }
     }
-  }
 }
 
 const InnerLayoutContext = React.createContext<Preferences>(getDefaultPreferences());
 
 
 const iconStyle = {
-  width: "2rem",
-  height: "2rem",
+    width: "2rem",
+    height: "2rem",
 };
 
 
 interface SidebarEntryProps {
-  label: string,
-  icon: Icon,
-  href: string,
+    label: string,
+    icon: Icon,
+    href: string,
 }
 
 const SidebarEntry: React.FunctionComponent<SidebarEntryProps> = props => {
-  const style = {
-    color: "#fff"
-  }
-  const Icon = props.icon;
-  if (React.useContext(InnerLayoutContext).collapsed) {
-    // collapsed
-    return <a style={style} className="nav-item nav-link" href={props.href}>
-      <Icon style={iconStyle} />
-    </a>
-  } else {
-    // not collapsed
-    return <a style={style} className="nav-item nav-link" href={props.href}>
-      <Icon style={iconStyle} className="me-2" />{props.label}
-    </a>
-  }
+    const style = {
+        color: "#fff"
+    }
+    const Icon = props.icon;
+    if (React.useContext(InnerLayoutContext).collapsed) {
+        // collapsed
+        return <a style={style} className="nav-item nav-link" href={props.href}>
+            <Icon style={iconStyle} />
+        </a>
+    } else {
+        // not collapsed
+        return <a style={style} className="nav-item nav-link" href={props.href}>
+            <Icon style={iconStyle} className="me-2" />{props.label}
+        </a>
+    }
 }
 
 const loadUserData = async (props: AsyncProps<UserData>) => {
-  const userData = await userDataView({
-    creatorUserId: [props.apiKey.creatorUserId],
-    onlyRecent: true,
-    apiKey: props.apiKey.key,
-  })
-    .then(unwrap);
+    const userData = await userDataView({
+        creatorUserId: [props.apiKey.creatorUserId],
+        onlyRecent: true,
+        apiKey: props.apiKey.key,
+    })
+        .then(unwrap);
 
-  return userData[0];
+    return userData[0];
 }
 
 
@@ -89,121 +89,122 @@ const loadUserData = async (props: AsyncProps<UserData>) => {
 const Body: React.FunctionComponent = props => <> {props.children} </>
 
 interface InnerLayoutComposition {
-  SidebarEntry: React.FunctionComponent<SidebarEntryProps>
-  Body: React.FunctionComponent
+    SidebarEntry: React.FunctionComponent<SidebarEntryProps>
+    Body: React.FunctionComponent
 }
 
 interface InnerLayoutProps {
-  apiKey: ApiKey
-  logoutCallback: () => void
+    apiKey: ApiKey
+    logoutCallback: () => void
 }
 
 
 
 const InnerLayout: React.FunctionComponent<React.PropsWithChildren<InnerLayoutProps>> & InnerLayoutComposition =
-  props => {
-  const [preferences, setPreferencesState] = React.useState(getPreexistingPreferences());
-  const setPreferences = (data: Preferences) => {
-    localStorage.setItem("preferences", JSON.stringify(data));
-    setPreferencesState(data);
-      };
+    props => {
+        const [preferences, setPreferencesState] = React.useState(getPreexistingPreferences());
+        const setPreferences = (data: Preferences) => {
+            localStorage.setItem("preferences", JSON.stringify(data));
+            setPreferencesState(data);
+        };
 
-      function submit() {
-          return confirmAlert({
-              title: 'Title',                        // Title dialog
-              message: 'Are you sure you want to log out?',               // Message dialog
-              buttons: [
-                  {
-                      label: 'Yes',
-                      onClick: () => props.logoutCallback()
-                  },
-                  {
-                      label: 'No',
-                      onClick: () => alert('Click No')
-                  }
-              ]
-          })
-      };
+        function submit() {
+            console.log("called submit");
+            return confirmAlert({
+                title: 'Title',                        // Title dialog
+                message: 'Are you sure you want to log out?',               // Message dialog
+                buttons: [
+                    {
+                        label: 'Yes',
+                        onClick: () => props.logoutCallback()
+                    },
+                    {
+                        label: 'No',
+                        onClick: () => alert('Click No')
+                    }
+                ]
+            })
+        };
 
-    const widthrem = preferences.collapsed ? 4 : 15;
+        const widthrem = preferences.collapsed ? 4 : 15;
 
-    const sidebarStyle = {
-      height: "100%",
-      width: `${widthrem}rem`,
-      position: "fixed" as const,
-      top: 0,
-      left: 0,
-      overflowX: "hidden" as const,
-      overflowY: "hidden" as const,
-      margin: "0%"
-    };
+        const sidebarStyle = {
+            height: "100%",
+            width: `${widthrem}rem`,
+            position: "fixed" as const,
+            top: 0,
+            left: 0,
+            overflowX: "hidden" as const,
+            overflowY: "hidden" as const,
+            margin: "0%"
+        };
 
-    const sidebarBottom = {
-      position: 'absolute' as const,
-      bottom: 0,
-      right: 0,
-      left: 0,
-    };
+        const sidebarBottom = {
+            position: 'absolute' as const,
+            bottom: 0,
+            right: 0,
+            left: 0,
+        };
 
-    let sidebarChildren: React.ReactElement[] = [];
-    let nonSidebarChildren: React.ReactNode[] = [];
+        let sidebarChildren: React.ReactElement[] = [];
+        let nonSidebarChildren: React.ReactNode[] = [];
 
 
-    React.Children.forEach(props.children, child => {
-      if (React.isValidElement(child)) {
-        if (child.type === SidebarEntry) {
-          sidebarChildren.push(child);
-        } else if (child.type === Body) {
-          nonSidebarChildren.push(child);
-        }
-      }
-    });
+        React.Children.forEach(props.children, child => {
+            if (React.isValidElement(child)) {
+                if (child.type === SidebarEntry) {
+                    sidebarChildren.push(child);
+                } else if (child.type === Body) {
+                    nonSidebarChildren.push(child);
+                }
+            }
+        });
 
-    return (
-      <InnerLayoutContext.Provider value={preferences}>
-        <nav className="bg-dark text-light" style={sidebarStyle}>
-          <div className="nav-item nav-link link-light">
-            <MenuIcon style={iconStyle}
-              onClick={_ => setPreferences({
-                collapsed: !preferences.collapsed,
-                dark: preferences.dark
-              })}
-            />
-          </div>
-          <span className="nav-item nav-link link-light mx-auto my-3">
-            <Async promiseFn={loadUserData} apiKey={props.apiKey}>
-              <Async.Pending>
-                {preferences.collapsed ? false : <Loader />}
-              </Async.Pending>
-              <Async.Rejected>
-                <span className="text-danger">Couldn't load User</span>
-              </Async.Rejected>
-              <Async.Fulfilled<UserData>>{ud =>
-                preferences.collapsed
-                  ? false
-                  : <h6>Welcome, {ud.name}</h6>
-              }
-              </Async.Fulfilled>
-            </Async>
-          </span>
-          {sidebarChildren}
-          <div style={sidebarBottom}>
-            <button
-              type="button"
-              className="btn nav-item nav-link link-light"
-                        onClick={() => submit}
-            >
-              <ExitAppIcon style={iconStyle} className="me-2" />
-              {preferences.collapsed ? false : "Log Out"}
-            </button>
-          </div>
-        </nav>
-        <div style={{ marginLeft: `${widthrem}rem` }}>
-          {nonSidebarChildren}
-        </div>
-      </InnerLayoutContext.Provider>
-    )
-  }
+        return (
+            <InnerLayoutContext.Provider value={preferences}>
+                <nav className="bg-dark text-light" style={sidebarStyle}>
+                    <div className="nav-item nav-link link-light">
+                        <MenuIcon style={iconStyle}
+                            onClick={_ => setPreferences({
+                                collapsed: !preferences.collapsed,
+                                dark: preferences.dark
+                            })}
+                        />
+                    </div>
+                    <span className="nav-item nav-link link-light mx-auto my-3">
+                        <Async promiseFn={loadUserData} apiKey={props.apiKey}>
+                            <Async.Pending>
+                                {preferences.collapsed ? false : <Loader />}
+                            </Async.Pending>
+                            <Async.Rejected>
+                                <span className="text-danger">Couldn't load User</span>
+                            </Async.Rejected>
+                            <Async.Fulfilled<UserData>>{ud =>
+                                preferences.collapsed
+                                    ? false
+                                    : <h6>Welcome, {ud.name}</h6>
+                            }
+                            </Async.Fulfilled>
+                        </Async>
+                    </span>
+                    {sidebarChildren}
+                    <div style={sidebarBottom}>
+                        <button
+                            type="button"
+                            className="btn nav-item nav-link link-light"
+                            onClick={() => submit()}
+                        >
+                            <ExitAppIcon style={iconStyle} className="me-2" />
+                            {preferences.collapsed ? false : "Log Out"}
+                        </button>
+                    </div>
+                </nav>
+                <div style={{ marginLeft: `${widthrem}rem` }}>
+                    {nonSidebarChildren}
+                </div>
+            </InnerLayoutContext.Provider>
+        )
+    }
 
 InnerLayout.SidebarEntry = SidebarEntry;
 InnerLayout.Body = Body;
