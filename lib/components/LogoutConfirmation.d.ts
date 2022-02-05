401e14b17c9b9bd3d2c0860ace6ffe07cc491eb1
warrickhe <43077069+warrickhe@users.noreply.github.com>
2022-02-05 03:20:28 -0800

    Ran npm build

---
+++ lib/components/LogoutConfirmation.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+export declare const LogoutConfirmation: ({ isOpen, cancel, close }: {
+    isOpen: boolean;
+    cancel: any;
+    close: any;
+}) => React.ReactPortal | undefined;
